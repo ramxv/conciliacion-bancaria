53cc9c86feed23305a32aa33caaf8084dc4be273
ramxv <ramxv.singh@gmail.com>
2024-03-27 08:49:14 -0500

    Add validation functions on script.js and Add validations to cheques.html and anulacion.html

--- js/script.js
+++ js/script.js
@@ -7,3 +7,39 @@ function loadPage(page) {
         })
         .catch(error => console.error('Error al cargar la página:', error));
 }
+
+// * Función para restringir números en campos de nombre
+function soloLetras(evento) {
+    var code = (evento.which) ? evento.which : evento.keycode;
+    if (code == 8 || code == 32) {
+        return true;
+    } else if (code >= 65 && code <= 90 || code >= 97 && code <= 122) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// * Función para restringir letras de campos números
+function soloNumeros(evento) {
+    var code = (evento.which) ? evento.which : evento.keycode;
+    if (code == 8) {
+        return true;
+    } else if (code >= 48 && code <= 57) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// * Función acepta punto decimal en los campos de decimales
+function soloDecimal(evento) {
+    var code = (evento.which) ? evento.which : evento.keycode;
+    if (code == 8) {
+        return true;
+    } else if (code == 46 || code >= 48 && code <= 57) {
+        return true;
+    } else {
+        return false;
+    }
+}
