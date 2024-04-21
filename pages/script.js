cb5806f52ef9e063843c0e28fdbf0ff033244f0b
ramxv <ramxv.singh@gmail.com>
2024-04-21 11:48:31 -0500

    Refactor anulacion.php to ajax

--- pages/script.js
+++ pages/script.js
@@ -56,10 +56,10 @@ function validarNumCheque() {
 					console.log(response);
 					if (response.successNumCk) {
 						// El número de cheque es válido
-						$('.error-container').html('<div class="alert alert-success" role="alert">'+ response.mensajeNumCk +'</div>');
+						$('.error-container').html('<div class="alert alert-success" role="alert">' + response.mensajeNumCk + '</div>');
 						enableFields();
 					} else {
-						$('.error-container').html('<div class="alert alert-danger" role="alert"> '+ response.mensajeNumCk +' </div>');
+						$('.error-container').html('<div class="alert alert-danger" role="alert"> ' + response.mensajeNumCk + ' </div>');
 						disableFields();
 					}
 				} catch (error) {
@@ -85,71 +85,87 @@ function validarNumCheque() {
 // * Función para grabar Anulación
 function grabarAnulacion(event) {
 	event.preventDefault();
-	let form_data = new FormData(document.getElementById("anulacion-form"));
+	let fecha_anulacion = document.getElementById("fecha-anulada").value;
+	let detalle_anulacion = document.getElementById("detalle-anulacion").value;
+	// Crear un objeto FormData con los campos necesarios para la actualización
+	let formData = new FormData(document.getElementById("anulacion-form"));
 
+	if (fecha_anulacion === '' || detalle_anulacion === '') {
+		$('#mensaje-cliente').html('<div class="alert alert-warning" role="alert"> ⚠️ Llene todos los campos.</div>');
+	}
+
+	// Realizar la solicitud AJAX
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4 && xhr.status === 200) {
 			try {
-				console.log(xhr.responseText);
+				// Registrar la respuesta completa del servidor
+				console.log('Respuesta completa del servidor:', xhr.responseText);
+
+				// Analizar la respuesta JSON del servidor
 				var response = JSON.parse(xhr.responseText);
-				console.log(response);
+
 				if (response.success) {
-					console.log('Respuesta del servidor:', response.mensaje);
-					$('.error-container').html('<div class="alert alert-success" role="alert">' + response.mensaje + '</div>');
+					// Mostrar mensaje de éxito si se completó la actualización
+					console.log('Cheque anulado correctamente:', response.mensaje);
+					$('#mensaje-cliente').html('<div class="alert alert-success" role="alert">' + response.mensaje + '</div>');
 				} else {
-					console.error('Error en el servidor:', response.error);
-					$('.error-container').html('<div class="alert alert-danger" role="alert">' + response.error + '</div>');
+					// Mostrar mensaje de error si hubo problemas en la actualización
+					console.error('Error en el servidor:', response.mensaje || 'Error desconocido');
+					$('#mensaje-cliente').html('<div class="alert alert-danger" role="alert">' + (response.mensaje || 'Error desconocido') + '</div>');
 				}
 			} catch (e) {
 				console.error('Error al analizar la respuesta del servidor:', e);
+				$('#mensaje-cliente').html('<div class="alert alert-danger" role="alert">Error al analizar la respuesta del servidor</div>');
 			}
+		} else {
+			// Manejo de errores HTTP
+			console.error('Error HTTP:', xhr.status, xhr.statusText);
+			$('#mensaje-cliente').html('<div class="alert alert-danger" role="alert">Error en la solicitud HTTP</div>');
 		}
 	};
-	xhr.open('POST', 'logica_anulacion.php', true);
-	xhr.send(form_data);
+	// Configurar la solicitud AJAX como POST
+	xhr.open('POST', 'logica_grabarAnulacion.php', true);
+	// Enviar los datos de los campos para la actualización
+	xhr.send(formData);
 }
 
 // * Función para validar número de Cheque
 function validarCkAnulacion() {
 	let numeroCheque = document.getElementById("num-cheque-input").value.trim();
-	console.log(numeroCheque);
 	if (numeroCheque !== "") {
-		console.log(numeroCheque);
 		$.ajax({
 			type: 'POST',
-			url: 'logica_anulacion.php',
-			dataType: 'json', // Especifica que esperamos una respuesta JSON
+			url: 'logica_validarAnulacion.php',
+			dataType: 'json',
 			data: { numeroCheque: numeroCheque },
 			success: function (response) {
 				try {
-					console.log(response);
 					if (response.successCirculacion && response.successAnulado) {
 						// El número de cheque es válido
-						$('.error-container').html('<div class="alert alert-success" role="alert">'+ response.mensajeNumCk +'</div>');
+						$('#mensaje-cliente').html('<div class="alert alert-success" role="alert">' + response.mensajeNumCk + '</div>');
+						llenarCampos(response);
 						disableFields();
 					} else {
-						$('.error-container').html('<div class="alert alert-danger" role="alert"> '+ response.mensajeNumCk +' </div>');
-						disableFields();
+						$('#mensaje-cliente').html('<div class="alert alert-danger" role="alert">' + response.mensajeNumCk + '</div>');
 					}
 				} catch (error) {
 					console.error('Error al analizar la respuesta JSON:', error);
-					$('.error-container').html('<div class="alert alert-danger" role="alert">Error al analizar la respuesta del servidor</div>');
-					disableFields();
+					$('#mensaje-cliente').html('<div class="alert alert-danger" role="alert">Error al analizar la respuesta del servidor</div>');
 				}
 			},
 			error: function (xhr, status, error) {
-				console.error(error);
-				$('.error-container').html('<div class="alert alert-danger" role="alert">Error al conectar con el servidor</div>');
-				disableFields();
+				console.error('Error al conectar con el servidor:', error);
+				$('#mensaje-cliente').html('<div class="alert alert-danger" role="alert">Error al conectar con el servidor</div>');
 			}
 		});
 	} else {
 		// Error: número de cheque no recibido
-		$('.error-container').html('<div class="alert alert-danger" role="alert">Error: número de cheque no recibido.</div>');
-		disableFields();
+		$('#mensaje-cliente').html('<div class="alert alert-danger" role="alert">Error: número de cheque no recibido.</div>');
 	}
 }
+
+
 
 // ! ======================================== 			Sección Sacar de Circulación 				==============================================
 
@@ -163,9 +179,27 @@ function validarCkAnulacion() {
 
 
 
-// ! ======================================== 			Sección Resetear Mensaje Cliente 		==============================================
+// ! ======================================== 			Sección Funciones		==============================================
 
 function resetMensaje() {
 	const mensajeCliente = document.getElementById('mensaje-cliente');
 	mensajeCliente.innerHTML = '';
+}
+
+// * Esta funcón llena los campos del formulario con los datos recibidos
+function llenarCampos(response) {
+	$('#fecha-input').val(response.fecha);
+	$('#inputOrden').val(response.beneficiario);
+	$('#inputMonto').val(response.monto);
+	$('#inputDetalle').val(response.descripcion);
+	$('#fecha-anulada').val(response.fecha_anulado);
+}
+
+function vaciarCampos() {
+	// Vacía los campos del formulario
+	$('#fecha-input').val('');
+	$('#inputOrden').val('');
+	$('#inputMonto').val('');
+	$('#inputDetalle').val('');
+	$('#fecha-anulada').val('');
 }
--- pages/ui_anulacion.php
+++ pages/ui_anulacion.php
@@ -1,71 +1,71 @@
 <?php require "../php/db_conciliacion.php"; ?>
-<?php require "logica_anulacion.php"; ?>
+<?php require "logica_validarAnulacion.php"; ?>
+<?php require "logica_grabarAnulacion.php"; ?>
 <?php require "logica_cheque.php"; ?>
 
 <div class="container">
-  <form id="anulacion-form" class="border border-secondary-subtle rounded" method="POST" onsubmit="grabarAnulacion(event)">
-    <h2 class="form-header p-3 border-bottom border-secondary-subtle">Anulación de Cheque</h2>
-    <div class="row p-4">
-      <!-- Mensaje de error al usuario -->
-      <div class="error-container" id="mensaje-cliente"></div>
-      <!-- Contenedor de Anulación Cheque -->
-      <div class="col p-3">
-        <div class="anul-container">
-          <!-- Sección buscar cheque -->
-          <div class="row justify-content-end align-items-end">
-            <div class="col-4">
-              <label for="num-cheque-input" class="form-label">No.Cheque</label>
-              <input type="text" class="form-control" autocomplete="off" name="numeroCheque" id="num-cheque-input" value="<?= $numero_cheque ?>" onkeypress="return soloNumeros(event)">
-            </div>
-            <div class="col-4" id="btn-custom-container">
-              <button type="button" class="btn button-custom" onclick="validarCkAnulacion()" id="btn-buscar" name="buscar">Buscar</button>
-            </div>
-          </div>
-          <!-- Sección insertar datos desde la BD -->
-          <div class="col-4">
-            <label for="fecha-input" class="form-label">Fecha</label>
-            <input type="date" name="date" id="fecha-input" class="form-control" value="<?= $fecha ?>">
-          </div>
-          <div class="col-12 pt-2">
-            <label for="inputOrden" class="form-label">Páguese a la orden de</label>
-            <select class="form-select" id="inputOrden">
-              <?php if($proveedores->rowCount() > 0):?>
-                <?php while ($consultaProveedores = $proveedores->fetch(PDO::FETCH_ASSOC)): 
-                  $codigo = $consultaProveedores["codigo"];
-                  $nombre = $consultaProveedores["nombre"];
-                ?>
-                  <?php if($codigo == $beneficiario):?>
-                    <option><?= $nombre ?></option>
-                  <?php endif ?>
-                <?php endwhile ?>
-              <?php endif ?>
-            </select>
-          </div>
-          <div class="col-4 pt-2">
-            <label for="inputMonto" class="form-label">La suma de</label>
-            <div class="input-group">
-              <input type="text" aria-label="First name" class="form-control" id="inputMonto" placeholder="$" value="<?= $monto ?>">
-            </div>
-          </div>
-          <div class="col-12 pt-2">
-            <label for="inputDetalle" class="form-label">Detalle</label>
-            <input type="text" class="form-control" id="inputDetalle" placeholder="" value="<?= $descripcion ?>">
-          </div>
-        </div>
-      </div>
-      <div class="col p-3">
-        <div class="col-4">
-          <label for="fecha-input" class="form-label">Fecha de Anulación</label>
-            <input type="date" name="fechaAnulado" id="fecha-anulada" class="form-control" value="<?= $fecha_anulado ?>"> 
-        </div>
-        <div class="col-12 pt-2">
-          <label for="floatingTextarea2" class="form-label">Detalle de la Anulación</label>
-					<textarea class="form-control" placeholder="" id="floatingTextarea2" name="detalle_anulado" style="height: 100px"></textarea>
-        </div>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-center pt-5" id="btn-custom-container">
-          <button type="submit" class="btn button-custom" id="btn-anular" name="anular">Anular</button>
-        </div>
-      </div>
-    </div>
-  </form>
+	<form id="anulacion-form" class="border border-secondary-subtle rounded" method="POST" onsubmit="grabarAnulacion(event)">
+		<h2 class="form-header p-3 border-bottom border-secondary-subtle">Anulación de Cheque</h2>
+		<div class="row p-4">
+			<!-- Mensaje de error al usuario -->
+			<div class="error-container" id="mensaje-cliente"></div>
+			<!-- Contenedor de Anulación Cheque -->
+			<div class="col p-3">
+				<div class="anul-container">
+					<!-- Sección buscar cheque -->
+					<div class="row justify-content-end align-items-end">
+						<div class="col-4">
+							<label for="num-cheque-input" class="form-label">No.Cheque</label>
+							<input type="text" class="form-control" autocomplete="off" name="numeroCheque" id="num-cheque-input" value="<?= $numero_cheque ?>" onkeypress="return soloNumeros(event)">
+						</div>
+						<div class="col-4" id="btn-custom-container">
+							<button type="button" class="btn button-custom" onclick="validarCkAnulacion()" id="btn-buscar" name="buscar">Buscar</button>
+						</div>
+					</div>
+					<!-- Sección insertar datos desde la BD -->
+					<div class="col-4">
+						<label for="fecha-input" class="form-label">Fecha</label>
+						<input type="date" name="date" id="fecha-input" class="form-control" value="<?= $fecha ?>" disabled>
+					</div>
+					<div class="col-12 pt-2">
+						<label for="inputOrden" class="form-label">Páguese a la orden de</label>
+						<select class="form-select" id="inputOrden" disabled>
+							<option value=""></option>
+							<?php if ($proveedores->rowCount() > 0) : ?>
+								<?php while ($consultaProveedores = $proveedores->fetch(PDO::FETCH_ASSOC)) :
+									$codigo = $consultaProveedores["codigo"];
+									$nombre = $consultaProveedores["nombre"]; ?>
+									<option value="<?= $codigo ?>" <?= ($codigo == $beneficiario) ? 'selected' : '' ?>><?= $nombre ?></option>
+								<?php endwhile; ?>
+							<?php endif; ?>
+						</select>
+					</div>
+					<div class="col-4 pt-2">
+						<label for="inputMonto" class="form-label">La suma de</label>
+						<div class="input-group">
+							<input type="text" aria-label="First name" class="form-control" id="inputMonto" placeholder="$" value="<?= $monto ?>" disabled>
+						</div>
+					</div>
+					<div class="col-12 pt-2">
+						<label for="inputDetalle" class="form-label">Detalle</label>
+						<input type="text" class="form-control" id="inputDetalle" placeholder="" value="<?= $descripcion ?>" disabled>
+					</div>
+				</div>
+			</div>
+			<div class="col p-3">
+				<div class="col-4">
+					<label for="fecha-anulada" class="form-label">Fecha de Anulación</label>
+					<input type="date" name="fechaAnulado" id="fecha-anulada" class="form-control">
+				</div>
+				<div class="col-12 pt-2">
+					<label for="detalle-anulacion" class="form-label">Detalle de la Anulación</label>
+					<textarea class="form-control" placeholder="" id="detalle-anulacion" name="detalle_anulado" style="height: 100px"></textarea>
+				</div>
+				<div class="d-grid gap-2 d-md-flex justify-content-md-center pt-5" id="btn-custom-container">
+					<button type="submit" class="btn button-custom" id="btn-anular" name="anular">Anular</button>
+					<button type="reset" class="btn button-custom"  onclick="resetMensaje()">Limpiar</button>
+				</div>
+			</div>
+		</div>
+	</form>
 </div>
